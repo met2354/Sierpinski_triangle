f419b78f7952857af4c6cac11377c44bc5768f19
mihael <met2354@icloud.com>
2018-04-24 15:55:53 +0300

    0.0.1

---
+++ Sierpinski_triangle/FMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sierpinski_triangle
+{
+    public partial class FormMain : Form
+    {
+        int n = 1000;
+        float x;
+        float y;
+        float width = 10;
+        float height = 10;
+        float WidthScreen = SystemInformation.VirtualScreen.Width;//Ширина экрана
+        float HeightScreen = SystemInformation.VirtualScreen.Height;//Высота экрана
+        SolidBrush mySolidBrush = new SolidBrush(Color.Red);
+
+        public FormMain()
+        {
+            WindowState = FormWindowState.Maximized;
+            InitializeComponent();
+        }
+
+        private void FormMain_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics A = CreateGraphics();
+            Graphics B = CreateGraphics();
+            Graphics C = CreateGraphics();
+            Graphics RndPoint = CreateGraphics();
+            float xA = WidthScreen / 2 - width / 2;
+            float yA = 20 - height / 2;
+            float xB = xA - (HeightScreen - 100) / (float)(Math.Sqrt(3));
+            float yB = HeightScreen - 100 + 20 - height / 2;
+            float xC = xA + (HeightScreen - 100) / (float)(Math.Sqrt(3));
+            float yC = yB;
+            //рисует вершины равностороннего треугольника
+            A.FillEllipse(mySolidBrush, xA, yA, width, height);
+            B.FillEllipse(mySolidBrush, xB, yB, width, height);
+            C.FillEllipse(mySolidBrush, xC, yC, width, height);
+            //--
+            mySolidBrush.Color = Color.Black;
+            x = xA;
+            y = yA + 1;
+            RndPoint.FillEllipse(mySolidBrush, x, y, width, height);
+            Random rndValue = new Random();
+            int rnd;
+            for (int i = 0; i < n; i++)
+            {
+                //Thread.Sleep(500);
+                switch (rnd = rndValue.Next(-1, 3))
+                {
+                    case 0:
+                        RndPoint.FillEllipse(mySolidBrush, x = (x + xA) / 2, y = (y + yA) / 2, width, height);
+                        break;
+                    case 1:
+                        RndPoint.FillEllipse(mySolidBrush, x = (x + xB) / 2, y = (y + yB) / 2, width, height);
+                        break;
+                    case 2:
+                        RndPoint.FillEllipse(mySolidBrush, x = (x + xC) / 2, y = (y + yC) / 2, width, height);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Sierpinski_triangle/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Sierpinski_triangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sierpinski_triangle")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b87c0d95-a557-49b4-be40-714d85ea8be6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
